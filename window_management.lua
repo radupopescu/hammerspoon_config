e9ee3dbf07bc605df89ea0f6bf7328a5e210d1e7
Radu Popescu <mail@radupopescu.net>
2016-07-21 10:38:51 +0200

    Refactor window_management.lua to use hs.grid

--- window_management.lua
+++ window_management.lua
@@ -13,113 +13,132 @@ local keys = {
 
 -- No need to edit below this line
 
-local layouts = {
-    full = hs.layout.maximized,
-    left30 = hs.layout.left30,
-    left50 = hs.layout.left50,
-    left70 = hs.layout.left70,
-    right30 = hs.layout.right30,
-    right50 = hs.layout.right50,
-    right70 = hs.layout.right70,
-    top30 = {x = 0, y = 0, w = 1, h = 0.5},
-    top50 = {x = 0, y = 0, w = 1, h = 0.33333},
-    top70 = {x = 0, y = 0, w = 1, h = 0.66666},
-    bottom30 = {x = 0, y = 0.5, w = 1, h = 0.5},
-    bottom50 = {x = 0, y = 0.66666, w = 1, h = 0.33333},
-    bottom70 = {x = 0, y = 0.33333, w = 1, h = 0.66666},
-    tl = {x = 0, y = 0, w = 0.5, h = 0.5},
-    tr = {x = 0.5, y = 0, w = 0.5, h = 0.5},
-    bl = {x = 0, y = 0.5, w = 0.5, h = 0.5},
-    br = {x = 0.5, y = 0.5, w = 0.5, h = 0.5}
+local my_grid = hs.grid.setGrid('6x6', 'Color LCD').setMargins('0,0')
+
+local positions = {
+   left33 = hs.geometry.rect(0,0,2,6),
+   left50 = hs.geometry.rect(0,0,3,6),
+   left66 = hs.geometry.rect(0,0,4,6),
+   right33 = hs.geometry.rect(4,0,2,6),
+   right50 = hs.geometry.rect(3,0,3,6),
+   right66 = hs.geometry.rect(2,0,4,6),
+   top33 = hs.geometry.rect(0,0,6,2),
+   top50 = hs.geometry.rect(0,0,6,3),
+   top66 = hs.geometry.rect(0,0,6,4),
+   bottom33 = hs.geometry.rect(0,4,6,2),
+   bottom50 = hs.geometry.rect(0,3,6,3),
+   bottom66 = hs.geometry.rect(0,2,6,4),
+   tl = hs.geometry.rect(0,0,3,3),
+   tr = hs.geometry.rect(3,0,3,3),
+   bl = hs.geometry.rect(0,3,3,3),
+   br = hs.geometry.rect(3,3,3,3)
 }
 
-local eps = 10
-
-local function makeSetFrameCallback(coeff)
-   return function()
-      local win = hs.window.focusedWindow()
-      local f = win:frame()
-      local screen = win:screen()
-      local max = screen:frame()
-
-      f.x = max.x + coeff.x * max.w
-      f.y = max.y + coeff.y * max.h
-      f.w = coeff.w * max.w
-      f.h = coeff.h * max.h
-
-      win:setFrame(f)
-   end
+local function areCellsEqual(c1, c2)
+   return c1.x == c2.x and c1.y == c2.y and c1.w == c2.w and c1.h == c2.h
 end
 
-local function makeSetFrameCallback2(coeff)
+local function positionHelper(position_list)
    return function()
       local win = hs.window.focusedWindow()
-      local f = win:frame()
-      local screen = win:screen()
-      local max = screen:frame()
 
-      local num_coeff = #coeff
-      local current_coeff_idx = 0
-      for idx, c in pairs(coeff) do
-         local ex = math.abs(f.x - (max.x + c.x * max.w)) < eps
-         local ey = math.abs(f.y - (max.y + c.y * max.h)) < eps
-         local ew = math.abs(f.w - (c.w * max.w)) < eps
-         local eh = math.abs(f.h - (c.h * max.h)) < eps
-         if ex and ey and ew and eh then
-            current_coeff_idx = idx
+      local num_pos = #position_list
+      local pos_curr_idx = 0
+
+      for idx, p in pairs(position_list) do
+         local curr_cell = hs.grid.get(win)
+         if areCellsEqual(curr_cell, p) then
+            pos_curr_idx = idx
          end
       end
 
-      local new_coeff = {}
-      if current_coeff_idx > 0 and current_coeff_idx < num_coeff then
-         new_coeff = coeff[current_coeff_idx + 1]
+      local new_pos = ''
+      if pos_curr_idx > 0 and pos_curr_idx < num_pos then
+         new_pos = position_list[pos_curr_idx + 1]
       else
-         new_coeff = coeff[1]
+         new_pos = position_list[1]
       end
 
-      f.x = max.x + new_coeff.x * max.w
-      f.y = max.y + new_coeff.y * max.h
-      f.w = new_coeff.w * max.w
-      f.h = new_coeff.h * max.h
-
-      win:setFrame(f)
+      my_grid.set(win, new_pos)
    end
 end
 
+--
+--       local num_coeff = #coeff
+--       local current_coeff_idx = 0
+--       for idx, c in pairs(coeff) do
+--          local ex = math.abs(f.x - (max.x + c.x * max.w)) < eps
+--          local ey = math.abs(f.y - (max.y + c.y * max.h)) < eps
+--          local ew = math.abs(f.w - (c.w * max.w)) < eps
+--          local eh = math.abs(f.h - (c.h * max.h)) < eps
+--          if ex and ey and ew and eh then
+--             current_coeff_idx = idx
+--          end
+--       end
+--
+--       local new_coeff = {}
+--       if current_coeff_idx > 0 and current_coeff_idx < num_coeff then
+--          new_coeff = coeff[current_coeff_idx + 1]
+--       else
+--          new_coeff = coeff[1]
+--       end
+--
+--       f.x = max.x + new_coeff.x * max.w
+--       f.y = max.y + new_coeff.y * max.h
+--       f.w = new_coeff.w * max.w
+--       f.h = new_coeff.h * max.h
+--
+--       win:setFrame(f)
+--    end
+-- end
+
 -- Maximize
-hs.hotkey.bind(keys.prefix, keys.maximize, makeSetFrameCallback(layouts.full))
+local function maximizeHelper()
+    my_grid.maximizeWindow(hs.window.focusedWindow())
+end
+
+hs.hotkey.bind(keys.prefix, keys.maximize, maximizeHelper)
 -- Also maximize, since I'm used to this from Caffeine
-hs.hotkey.bind(keys.prefix, "F", makeSetFrameCallback(layouts.full))
+hs.hotkey.bind(keys.prefix, "F", maximizeHelper)
 
 -- Send to left, full height
-hs.hotkey.bind(keys.prefix, keys.left, makeSetFrameCallback2({layouts.left50,
-                                                              layouts.left30,
-                                                              layouts.left70}))
+hs.hotkey.bind(keys.prefix, keys.left, positionHelper({positions.left50,
+                                                       positions.left33,
+                                                       positions.left66}))
 
 -- Send to right, full height
-hs.hotkey.bind(keys.prefix, keys.right, makeSetFrameCallback2({layouts.right50,
-                                                               layouts.right30,
-                                                               layouts.right70}))
+hs.hotkey.bind(keys.prefix, keys.right, positionHelper({positions.right50,
+                                                        positions.right33,
+                                                        positions.right66}))
 
 -- Send to top
-hs.hotkey.bind(keys.prefix, keys.top, makeSetFrameCallback2({layouts.top50,
-                                                             layouts.top30,
-                                                             layouts.top70}))
+hs.hotkey.bind(keys.prefix, keys.top, positionHelper({positions.top50,
+                                                      positions.top33,
+                                                      positions.top66}))
+
 
 -- Send to bottom
-hs.hotkey.bind(keys.prefix, keys.bottom, makeSetFrameCallback2({layouts.bottom50,
-                                                                layouts.bottom30,
-                                                                layouts.bottom70}))
+hs.hotkey.bind(keys.prefix, keys.bottom, positionHelper({positions.bottom50,
+                                                         positions.bottom33,
+                                                         positions.bottom66}))
 
 -- Send to upper left
-hs.hotkey.bind(keys.prefix, keys.top_left, makeSetFrameCallback(layouts.tl))
+hs.hotkey.bind(keys.prefix, keys.top_left, function()
+    my_grid.set(hs.window.focusedWindow(), positions.tl)
+end)
 
 -- Send to upper right
-hs.hotkey.bind(keys.prefix, keys.top_right, makeSetFrameCallback(layouts.tr))
+hs.hotkey.bind(keys.prefix, keys.top_right, function()
+    my_grid.set(hs.window.focusedWindow(), positions.tr)
+end)
 
 -- Send to lower left
-hs.hotkey.bind(keys.prefix, keys.bottom_left, makeSetFrameCallback(layouts.bl))
+hs.hotkey.bind(keys.prefix, keys.bottom_left, function()
+    my_grid.set(hs.window.focusedWindow(), positions.bl)
+end)
 
 -- Send to lower right
-hs.hotkey.bind(keys.prefix, keys.bottom_right, makeSetFrameCallback(layouts.br))
+hs.hotkey.bind(keys.prefix, keys.bottom_right, function()
+    my_grid.set(hs.window.focusedWindow(), positions.br)
+end)
 
